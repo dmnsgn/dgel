5fb9340db0b3984830eac0fdfea60cb7d7677bc5
Damien Seguin <dmnsgn@gmail.com>
2021-06-29 09:33:51 +0100

    build: remove web_modules from main

--- web_modules/gl-matrix.js
+++
@@ -1,4 +0,0 @@
-export { y as glMatrix, L as mat3, M as mat4, N as quat, O as vec2, P as vec3, Q as vec4 } from './common/vec2-5054ab4c.js';
-export { m as mat2, a as mat2d, q as quat2 } from './common/index-ac4cbebc.js';
-import './common/es.typed-array.float32-array-cc49d38f.js';
-import './common/typed-array-constructor-6a40afc1.js';
