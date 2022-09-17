70667117c68f48bab9f321e6694fde3d846ab08b
Damien Seguin <dmnsgn@gmail.com>
2022-09-17 17:05:26 +0100

    feat: saturate is now a builtin in wgsl

--- src/shaders/utils.wgsl.ts
+++ src/shaders/utils.wgsl.ts
@@ -1,9 +1,3 @@
-const SATURATE = /* wgsl */ `
-fn saturate(x: f32) -> f32 {
-  return clamp(x, 0.0, 1.0);
-}
-`;
-
 const RANDOM = /* wgsl */ `
 fn random(st: vec2<f32>) -> f32 {
   return fract(sin(dot(st.xy, vec2<f32>(12.9898, 78.233))) * 43758.5453123);
@@ -11,6 +5,5 @@ fn random(st: vec2<f32>) -> f32 {
 `;
 
 export default {
-  SATURATE,
   RANDOM,
 };
